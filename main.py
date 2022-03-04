7767ae5cb3ef9f90b4d7bebd698b566acd208cbf
Bharti20 <bhartikumari20@navgurukul.org>
2022-03-04 20:27:09 +0530

    created rock-paper-scissor game

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-options =['rock', 'paper', 'secisser']
+import random
+
+userChoice = input('Enter your choice:  rock, paper, scissor ')
+computerChoice = random.choice(['rock', 'paper', 'scissor'])
+print()
+if userChoice == computerChoice:
+    print('Game is draw!  because', 'computer chosen', computerChoice) 
+elif computerChoice == 'rock':
+    if userChoice == 'paper':
+        print('You won! because', 'computer chosen', computerChoice)
+    else:
+        print('Computer won! because', 'computer chosen', computerChoice) 
+elif computerChoice == 'paper':
+    if userChoice == 'scissor':
+        print('You won!  because', 'computer chosen', computerChoice) 
+    else:
+        print('Computer won!  because', 'computer chosen', computerChoice) 
+elif computerChoice == 'scissor':
+    if userChoice == 'rock':
+        print('You won!  because', 'computer chosen', computerChoice) 
+    else:
+        print('Computer won!  because', 'computer chosen', computerChoice) 
+else:
+    print('Please enter valid input') 
