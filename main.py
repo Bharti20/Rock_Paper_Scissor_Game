504e9293c20abeb532606c186cd02c2af4479d68
Bharti20 <bhartikumari20@navgurukul.org>
2022-03-04 22:58:37 +0530

    addes user chances feature

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import random
+draw = 0
+won = 0
+lost = 0
+while True:
+    userChoice = input('Enter your choice:  rock, paper, scissor ')
+    computerChoice = random.choice(['rock', 'paper', 'scissor'])
+    print()
+    if userChoice == computerChoice:
+        print('DRAW!')
+        draw+=1
+    elif computerChoice == 'rock':
+        if userChoice == 'paper':
+            print('YOU WON!  Paper Beats Rock')
+            won+=1
+        else:
+            print('YOU LOSE!  Rock Beats Scissor')
+            lost+=1
+    elif computerChoice == 'paper':
+        if userChoice == 'scissor':
+            print('YOU WON!  Scissor Beats paper')
+            won+=1
+        else:
+            print('YOU LOSE!   Paper Beats Rock')
+            lost+=1
+    elif computerChoice == 'scissor':
+        if userChoice == 'rock':
+            print('YOU WON!  Rock Beats Scissor')
+            won+=1
+        else:
+            print('YOU LOSE! Scissor Beats Paper')
+            lost+=1
+    else:
+        print('Please enter valid input')
+    
+    print()
+    userInput = input('For exit from game pres "exit" otherwise "Enter" once ')
+    if userInput == 'exit':
+        print('Won =', won,  'Lost =', lost,  'Draw =', draw)
+        break
 
-userChoice = input('Enter your choice:  rock, paper, scissor ')
-computerChoice = random.choice(['rock', 'paper', 'scissor'])
-print()
-if userChoice == computerChoice:
-    print('DRAW!') 
-elif computerChoice == 'rock':
-    if userChoice == 'paper':
-        print('YOU WON!  Paper Beats Rock')
-    else:
-        print('YOU LOSE!  Rock Beats Scissor') 
-elif computerChoice == 'paper':
-    if userChoice == 'scissor':
-        print('YOU WON!  Scissor Beats paper') 
-    else:
-        print('YOU LOSE!   Paper Beats Rock') 
-elif computerChoice == 'scissor':
-    if userChoice == 'rock':
-        print('YOU WON!  Rock Beats Scissor') 
-    else:
-        print('YOU LOSE! Scissor Beats Paper') 
-else:
-    print('Please enter valid input') 
+
